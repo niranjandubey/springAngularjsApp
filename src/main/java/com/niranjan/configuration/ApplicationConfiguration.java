ca42465de39859332e52a84a8173adb821f6273f
Niranjan Dubey <niranjandubey50@gmail.com>
2017-03-07 18:19:21 +0530

    file with initial commits

---
+++ src/main/java/com/niranjan/configuration/ApplicationConfiguration.java
@@ -0,0 +1,29 @@
+package com.niranjan.configuration;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configurable
+@EnableWebMvc
+@ComponentScan(basePackages = "com.niranjan")
+public class ApplicationConfiguration extends WebMvcConfigurerAdapter{
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        registry.viewResolver(viewResolver);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("/static/");
+    }
+}
---
+++ src/main/java/com/niranjan/configuration/ApplicationInitializer.java
@@ -0,0 +1,28 @@
+package com.niranjan.configuration;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
+
+public class ApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer{
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] { ApplicationConfiguration.class };
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        Filter [] singleton = { new CORSFilter() };
+        return singleton;
+    }
+}
